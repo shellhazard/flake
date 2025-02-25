c970494bd7c61d0f9742bd706b07946dbdc18319
shellhazard <shellhazard@tutanota.com>
2025-02-25 16:14:59 +1100

    another attempt at writing breezewiki module options

--- modules/breezewiki/default.nix
+++ modules/breezewiki/default.nix
@@ -12,41 +12,42 @@ in
     services.breezewiki = {
       enable = mkEnableOption "breezewiki";
       config = lib.mkOption {
-        type = types.submodule;
-        options = {
-          bind_host = mkOption {
-            type = types.str;
-            description = "Which hostname to run the server on. The default value is auto. Auto means 127.0.0.1 in debug mode, otherwise, all interfaces. If you don’t know what this means, don’t change it.";
-            default = "auto";
-          };
-          port = mkOption {
-            type = types.int;
-            description = "Which port to run the server on.";
-            default = 10416;
-          };
-          canonical_origin = mkOption {
-            type = types.str;
-            description = "The primary URL where people can access the homepage of your instance.";
-          };
-          debug = mkOption {
-            type = types.bool;
-            description = "Enables debugging mode, for debugging BreezeWiki during development. Enables more runtime checks and more verbose output. Turns off some browser caching.";
-            default = false;
-          };
-          feature_search_suggestions = mkOption {
-            type = types.bool;
-            description = "Enables the search suggestions feature. When enabled, any text typed in the search box will be sent to Fandom servers in order to provide suggestions. It will be sent via the proxy if the strict_proxy option is set.";
-            default = true;
-          };
-          log_outgoing = mkOption {
-            type = types.bool;
-            description = "Whether to log outgoing requests to Fandom to the console.";
-            default = true;
-          };
-          strict_proxy = mkOption {
-            type = types.bool;
-            description = "Whether to put more URLs through the proxy. If false, just a minimal set is proxied. If true, additionally proxies page stylesheets and links to image files, thereby reducing the potential for end-users to connect to Fandom servers.";
-            default = true;
+        type = types.submodule {
+          options = {
+            bind_host = mkOption {
+              type = types.str;
+              description = "Which hostname to run the server on. The default value is auto. Auto means 127.0.0.1 in debug mode, otherwise, all interfaces. If you don’t know what this means, don’t change it.";
+              default = "auto";
+            };
+            port = mkOption {
+              type = types.int;
+              description = "Which port to run the server on.";
+              default = 10416;
+            };
+            canonical_origin = mkOption {
+              type = types.str;
+              description = "The primary URL where people can access the homepage of your instance.";
+            };
+            debug = mkOption {
+              type = types.bool;
+              description = "Enables debugging mode, for debugging BreezeWiki during development. Enables more runtime checks and more verbose output. Turns off some browser caching.";
+              default = false;
+            };
+            feature_search_suggestions = mkOption {
+              type = types.bool;
+              description = "Enables the search suggestions feature. When enabled, any text typed in the search box will be sent to Fandom servers in order to provide suggestions. It will be sent via the proxy if the strict_proxy option is set.";
+              default = true;
+            };
+            log_outgoing = mkOption {
+              type = types.bool;
+              description = "Whether to log outgoing requests to Fandom to the console.";
+              default = true;
+            };
+            strict_proxy = mkOption {
+              type = types.bool;
+              description = "Whether to put more URLs through the proxy. If false, just a minimal set is proxied. If true, additionally proxies page stylesheets and links to image files, thereby reducing the potential for end-users to connect to Fandom servers.";
+              default = true;
+            };
           };
         };
       };
